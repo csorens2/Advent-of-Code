c6b4c797d43713461c82738cd8ba56a43b29eb59
BearNBird <csorens2@users.noreply.github.com>
2023-01-31 00:15:00 -0600

    Day1 Fix

--- Solutions/2019/Day1/out/Day1.js
+++ Solutions/2019/Day1/out/Day1.js
@@ -5,11 +5,11 @@ function get_input(filepath) {
     var file_contents = (0, fs_1.readFileSync)(filepath, 'utf-8');
     return file_contents.split(/\r?\n/).map(function (mass) { return parseInt(mass); });
 }
-function get_fuel_for_mass(mass) {
+function get_fuel(mass) {
     return Math.floor(mass / 3) - 2;
 }
 function rec_get_fuel(next_mass) {
-    var next_fuel_mass = get_fuel_for_mass(next_mass);
+    var next_fuel_mass = get_fuel(next_mass);
     if (next_fuel_mass < 0) {
         return 0;
     }
@@ -17,12 +17,11 @@ function rec_get_fuel(next_mass) {
 }
 var input_file_path = __dirname + "/../input.txt";
 var input = get_input(input_file_path);
-var fuel_per_module = input.map(get_fuel_for_mass);
+var fuel_per_module = input.map(get_fuel);
 var part_1 = fuel_per_module.reduce(function (acc, next) { return acc + next; });
 console.log("Part 1 Result ".concat(part_1));
 var part_2 = fuel_per_module.
     map(function (starter_fuel) { return starter_fuel + rec_get_fuel(starter_fuel); }).
     reduce(function (acc, next) { return acc + next; });
 console.log("Part 2 Result ".concat(part_2));
-var test = 1;
 //# sourceMappingURL=Day1.js.map
