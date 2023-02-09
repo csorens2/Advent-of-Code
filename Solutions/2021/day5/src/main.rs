30ecf3c599596bed553c0b63e5173c5dc8b73c0f
BearNBird <csorens2@users.noreply.github.com>
2023-02-09 08:56:09 -0800

    Day5 Complete

--- Solutions/2021/day5/src/main.rs
+++ Solutions/2021/day5/src/main.rs
@@ -1,17 +1,14 @@
-use std::collections::btree_map::Range;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use regex::Regex;
 use std::collections::HashMap;
-use std::cmp;
-use std::ops;
 
 fn main() {
     let input = read_input();
     let part_one = part_one(input.clone());
     println!("Part one result {}", part_one); // 7269
-    //let part_two = part_two(&input);
-    //println!("Part two result {}", part_two); 
+    let part_two = part_two(input.clone());
+    println!("Part two result {}", part_two); // 21140
 }
 
 fn read_input() -> Vec<((i32,i32),(i32,i32))> {
@@ -36,31 +33,56 @@ fn read_input() -> Vec<((i32,i32),(i32,i32))> {
 }
 
 fn part_one(input: Vec<((i32,i32),(i32,i32))>) -> i32 {
+    return process_points(input, false);
+}
+
+fn part_two(input: Vec<((i32,i32),(i32,i32))>) -> i32 {
+    return process_points(input, true);
+}
+
+fn process_points(input: Vec<((i32,i32),(i32,i32))>, process_diag: bool) -> i32 {
     let mut grid: HashMap<(i32,i32), bool> = HashMap::new();
     for ((x1,y1),(x2,y2)) in input {
-        let mut x_range: Vec<i32> = Vec::new();
-        let mut y_range: Vec<i32> = Vec::new();
-        if x1 != x2 && y1 != y2 {
+        if x1 != x2 && y1 != y2 && !process_diag {
             continue;
         }
-        if x1 == x2 {
-            x_range = vec![x1];
-        } 
-        else {
-            let x_min = cmp::min(x1,x2);
-            let x_max = cmp::max(x1,x2);
-            x_range = (x_min..x_max + 1).collect();
-        }
-        if y1 == y2 {
-            y_range = vec![y1];
+
+        let mut curr_x: i32;
+        let mut curr_y: i32;
+        let end_x: i32;
+        let end_y: i32;
+        if x1 <= x2 {
+            (curr_x, curr_y) = (x1,y1);
+            (end_x, end_y) = (x2,y2);
         }
         else {
-            let y_min = cmp::min(y1,y2);
-            let y_max = cmp::max(y1,y2);
-            y_range = (y_min..y_max + 1).collect();
+            (end_x, end_y) = (x1,y1);
+            (curr_x, curr_y) = (x2,y2);
         }
 
-        fill_grid(&x_range, &y_range, &mut grid);
+        let mut to_insert:Vec<(i32,i32)> = vec![(end_x, end_y)];
+        while (curr_x, curr_y) != (end_x, end_y) {
+            to_insert.push((curr_x, curr_y));
+            if curr_x != end_x {
+                curr_x += 1;
+            }
+
+            if curr_y < end_y {
+                curr_y += 1;
+            }
+            else if curr_y > end_y{
+                curr_y -= 1;
+            }
+        }
+
+        for (insert_x, insert_y) in to_insert {
+            if grid.contains_key(&(insert_x,insert_y)) {
+                grid.insert((insert_x,insert_y), true);
+            }
+            else {
+                grid.insert((insert_x,insert_y), false);
+            }
+        }
     }
 
     let overlap_count = 
@@ -70,21 +92,4 @@ fn part_one(input: Vec<((i32,i32),(i32,i32))>) -> i32 {
         len();
 
     return overlap_count as i32;
-}
-
-fn fill_grid(x_range: &Vec<i32>, y_range: &Vec<i32>, grid: &mut HashMap<(i32,i32), bool>) {
-    for x in x_range {
-        for y in y_range {
-            if grid.contains_key(&(*x,*y)) {
-                grid.insert((*x, *y), true);
-            }
-            else {
-                grid.insert((*x,*y), false);
-            }
-        }
-    }
-}
-
-fn part_two() -> i32 {
-    return 0;
 }
