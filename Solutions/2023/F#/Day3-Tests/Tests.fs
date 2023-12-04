87da689b12b0d55699afd5bd5052bf78ea6c09bd
BearNBird <csorens2@users.noreply.github.com>
2023-12-04 08:50:54 -0700

    Day3 Base Files

---
+++ Solutions/2023/F#/Day3-Tests/Tests.fs
@@ -0,0 +1,14 @@
+module Tests
+
+open Xunit
+open Day3
+
+[<Fact>]
+let ``Part1 SubTest`` () = 
+    let input = ParseInput("InputPart1.txt")
+    0
+
+[<Fact>]
+let ``Part2 SubTest`` () = 
+    let input = ParseInput("InputPart2.txt")
+    0
---
+++ Solutions/2023/F#/Day3/Program.fs
@@ -0,0 +1,16 @@
+﻿module Day3
+
+open System.IO
+
+let ParseInput filepath = 
+    File.ReadLines(filepath)
+    |> Seq.toList
+
+[<EntryPoint>]
+let main _ =
+    let input = ParseInput("Input.txt")
+    //let part1Result = Part1 input
+    //printfn "Part 1 Result: %d" part1Result // 
+    //let part2Result = Part2 input
+    //printfn "Part 2 Result: %d" part2Result // 
+    0
