a18fe9726e6fe30859647d35da4fed0402b551d7
BearNBird <csorens2@users.noreply.github.com>
2023-12-08 21:49:32 -0600

    Day 7 - Part 1 Complete

--- Solutions/2023/F#/Day7/Program.fs
+++ Solutions/2023/F#/Day7/Program.fs
@@ -1,16 +1,167 @@
 ﻿module Day7
 
 open System.IO
+open System.Text.RegularExpressions
+
+// Number indicates it's strength rank
+type CardType = 
+    | Ace = 1
+    | King = 2
+    | Queen = 3
+    | Jack = 4
+    | Ten = 5
+    | Nine = 6
+    | Eight = 7
+    | Seven = 8
+    | Six = 9
+    | Five = 10
+    | Four = 11
+    | Three = 12
+    | Two = 13
+    | Joker = 14
+
+// Number indicates it's strength rank
+type HandType = 
+    | FiveOfAKind = 1
+    | FourOfAKind = 2
+    | FullHouse = 3
+    | ThreeOfAKind = 4
+    | TwoPair = 5
+    | Pair = 6
+    | HighCard = 7
+
+type Hand = {
+    Cards: CardType list
+    Bid: int
+}
 
 let ParseInput filepath = 
     File.ReadLines(filepath)
+    |> Seq.map (fun line ->
+        let lineMatch = Regex(@"(\w+) (\d+)").Match line
+        let handCards = lineMatch.Groups.[1].Value
+        let bid = int lineMatch.Groups.[2].Value
+        (handCards, bid))
     |> Seq.toList
+
+let GetHandType toProcess = 
+
+    let cardCount = 
+        toProcess.Cards
+        |> List.fold (fun cardMap nextCard ->
+            // Make sure to not count jokers
+            match Map.tryFind nextCard cardMap with 
+            | Some _ when nextCard = CardType.Joker -> cardMap
+            | None -> Map.add nextCard 1 cardMap
+            | Some prevCount -> Map.add nextCard (1 + prevCount) cardMap) Map.empty
+
+    let cardCountValues = Map.values cardCount
+
+    let pairCount = 
+        cardCountValues
+        |> Seq.filter (fun count -> count = 2)
+        |> Seq.length
+
+    let tripleCount = 
+        cardCountValues
+        |> Seq.filter (fun count -> count = 3)
+        |> Seq.length
+
+    let fourOfExists = Option.isSome (Seq.tryFind (fun count -> count = 4) cardCountValues)
+
+    let fiveOfExists = Option.isSome (Seq.tryFind (fun count -> count = 5) cardCountValues)
+
+    if fiveOfExists then
+        HandType.FiveOfAKind
+    elif fourOfExists then
+        HandType.FourOfAKind
+    elif tripleCount = 1 && pairCount = 1 then
+        HandType.FullHouse
+    elif tripleCount = 1 then
+        HandType.ThreeOfAKind
+    elif pairCount = 2 then
+        HandType.TwoPair
+    elif pairCount = 1 then 
+        HandType.Pair
+    else
+        HandType.HighCard
+
+let CompareHands hand1 hand2 = 
+    let hand1Type = GetHandType hand1
+    let hand2Type = GetHandType hand2
+    match compare hand1Type hand2Type with 
+    | 0 -> 
+        let rec compareCardPairs (remainingCardPairs: (CardType*CardType) list) = 
+            match List.isEmpty remainingCardPairs with 
+            | true -> 0
+            | false -> 
+                let (card1, card2) = List.head remainingCardPairs
+                match compare card1 card2 with 
+                | 0 -> compareCardPairs (List.tail remainingCardPairs)
+                | cardTypeDiff -> cardTypeDiff
+        compareCardPairs (List.zip hand1.Cards hand2.Cards)
+    | handTypeDiff -> handTypeDiff 
+
+let Part1 (input: (string*int) list) = 
+    let cardCharMap = 
+        [
+            ('A', CardType.Ace)
+            ('K', CardType.King)
+            ('Q', CardType.Queen)
+
+            ('J', CardType.Jack)
+
+            ('T', CardType.Ten)
+            ('9', CardType.Nine)
+            ('8', CardType.Eight)
+            ('7', CardType.Seven)
+            ('6', CardType.Six)
+            ('5', CardType.Five)
+            ('4', CardType.Four)
+            ('3', CardType.Three)
+            ('2', CardType.Two)
+        ]
+        |> Map.ofList
+
+    input
+    |> List.map (fun (handString, bid) ->
+        let cardList = 
+            handString
+            |> Seq.map (fun handChar -> cardCharMap[handChar])
+            |> Seq.toList
+        {Hand.Cards = cardList; Bid = bid})
+    |> List.sortWith (fun hand1 hand2 -> -1 * (CompareHands hand1 hand2))
+    |> List.indexed
+    |> List.map (fun (index, hand) -> (index + 1, hand))
+    |> List.fold (fun totalWinnings (rank, hand) -> totalWinnings + (rank * hand.Bid)) 0 
+
+let Part2 input = 
+    let cardCharMap = 
+        [
+            ('A', CardType.Ace)
+            ('K', CardType.King)
+            ('Q', CardType.Queen)
+
+            ('J', CardType.Joker)
+
+            ('T', CardType.Ten)
+            ('9', CardType.Nine)
+            ('8', CardType.Eight)
+            ('7', CardType.Seven)
+            ('6', CardType.Six)
+            ('5', CardType.Five)
+            ('4', CardType.Four)
+            ('3', CardType.Three)
+            ('2', CardType.Two)
+        ]
+        |> Map.ofList
+    0
 
 [<EntryPoint>]
 let main _ =
     let input = ParseInput("Input.txt")
-    //let part1Result = Part1 input
-    //printfn "Part 1 Result: %d" part1Result // 
+    let part1Result = Part1 input
+    printfn "Part 1 Result: %d" part1Result // 251216224
     //let part2Result = Part2 input
     //printfn "Part 2 Result: %d" part2Result // 
     0
--- Solutions/2023/F#/Tests/Day7-Tests/Tests.fs
+++ Solutions/2023/F#/Tests/Day7-Tests/Tests.fs
@@ -4,21 +4,81 @@ open Xunit
 open Day7
 
 [<Fact>]
+let ``GetHandType returns correct HandType`` () = 
+    let baseHand = {Hand.Cards = List.empty; Bid = 0}
+    // Yes, these technically aren't actual poker hands since only two have 5 cards. 
+    // The function should still give us the results we want even with the incomplete hands, since the rest of the cards would be irrelevant.
+    // Besides, you should be able to implement this function without taking a dependency on needing 5 cards. 
+    let testCases = 
+        [
+            ({baseHand with Cards = List.empty}, HandType.HighCard)
+            ({baseHand with Cards = [CardType.Ace; CardType.Ace]}, HandType.Pair)
+            ({baseHand with Cards = [CardType.Ace; CardType.Ace; CardType.King; CardType.King]}, HandType.TwoPair)
+            ({baseHand with Cards = [CardType.Ace; CardType.Ace; CardType.Ace]}, HandType.ThreeOfAKind)
+            ({baseHand with Cards = [CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace]}, HandType.FourOfAKind)
+            ({baseHand with Cards = [CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace]}, HandType.FiveOfAKind)
+            ({baseHand with Cards = [CardType.Ace; CardType.Ace; CardType.Ace; CardType.King; CardType.King]}, HandType.FullHouse)
+        ]
+
+    for (testHand, expectedHandType) in testCases do
+        Assert.Equal(expectedHandType, GetHandType testHand)
+
+[<Fact>]
+let ``CompareHands properly compares different HandTypes`` () = 
+    let baseHand = {Hand.Cards = List.empty; Bid = 0}
+    let highCardHand = {baseHand with Cards = [CardType.Ace; CardType.King; CardType.Queen; CardType.Jack; CardType.Ten]}
+    let fiveOfAKindHand = {baseHand with Cards = [CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace]}
+    let rec testHands_Negative = ((fiveOfAKindHand, highCardHand), (fun checkNegative -> checkNegative < 0), nameof testHands_Negative)
+    let rec testHands_Positive = ((highCardHand, fiveOfAKindHand), (fun checkNegative -> 0 < checkNegative), nameof testHands_Positive)
+    
+    let testCases = [testHands_Negative; testHands_Positive]
+    for ((testHand1, testHand2), expectedValueFunc, testName) in testCases do 
+        Assert.True(expectedValueFunc (CompareHands testHand1 testHand2), $"Test '{testName}' failed.")
+    
+[<Fact>]
+let ``CompareHands properly compares using individual card strength when HandType is the same`` () = 
+    let baseHand = {Hand.Cards = List.empty; Bid = 0}
+
+    let rec testHands_CompareCard1_Negative = 
+        ((
+            {baseHand with Cards = [CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace]},
+            {baseHand with Cards = [CardType.King; CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace]}
+        ), (fun checkNegative -> checkNegative < 0),
+        nameof testHands_CompareCard1_Negative)
+
+    let rec testHands_CompareCard2_Negative = 
+        ((
+            {baseHand with Cards = [CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace]},
+            {baseHand with Cards = [CardType.Ace; CardType.King; CardType.Ace; CardType.Ace; CardType.Ace]}
+        ), (fun checkNegative -> checkNegative < 0),
+        nameof testHands_CompareCard2_Negative)
+
+    let rec testHands_CompareCard1_Positive = 
+        ((
+            {baseHand with Cards = [CardType.King; CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace]},
+            {baseHand with Cards = [CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace; CardType.Ace]}
+        ), (fun checkPositive -> 0 < checkPositive),
+        nameof testHands_CompareCard1_Positive)
+
+    let testCases = [testHands_CompareCard1_Negative; testHands_CompareCard2_Negative; testHands_CompareCard1_Positive]
+    for ((testHand1, testHand2), expectedValueFunc, testName) in testCases do 
+        Assert.True(expectedValueFunc (CompareHands testHand1 testHand2), $"Test '{testName}' failed.")
+        
+
+[<Fact>]
 let ``Part1 Example Test`` () = 
     let input = ParseInput("ExamplePart1.txt")
-    //Assert.Equal(, Part1 input)
-    Assert.Fail("Not implemented")
+    Assert.Equal(6440, Part1 input)
 
 [<Fact>]
 let ``Part1 Input Test`` () = 
     let input = ParseInput("Input.txt")
-    //Assert.Equal(, Part1 input)
-    Assert.Fail("Not implemented")
+    Assert.Equal(251216224, Part1 input)
 
 [<Fact>]
 let ``Part2 Example Test`` () = 
     let input = ParseInput("ExamplePart2.txt")
-    //Assert.Equal(, Part2 input)
+    Assert.Equal(5905, Part2 input)
     Assert.Fail("Not implemented")
 
 [<Fact>]
