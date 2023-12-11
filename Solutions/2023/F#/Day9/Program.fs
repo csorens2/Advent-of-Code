dd325e6016b9f9cad68e45a7fd7922420cc31510
BearNBird <csorens2@users.noreply.github.com>
2023-12-10 22:26:48 -0600

    Day9 Setup

---
+++ Solutions/2023/F#/Day9/Program.fs
@@ -0,0 +1,21 @@
+﻿module Day9
+
+open System.IO
+
+let ParseInput filepath = 
+    File.ReadLines(filepath)
+
+let Part1 input = 
+    0
+
+let Part2 input = 
+    0
+
+[<EntryPoint>]
+let main _ =
+    let input = ParseInput("Input.txt")
+    //let part1Result = Part1 input
+    //printfn "Part 1 Result: %d" part1Result // 
+    //let part2Result = Part2 input
+    //printfn "Part 2 Result: %d" part2Result // 
+    0
