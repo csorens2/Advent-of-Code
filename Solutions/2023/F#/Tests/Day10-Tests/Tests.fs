290c5f06bb95f8259e111fbe1ff1fe8e9ea26197
BearNBird <csorens2@users.noreply.github.com>
2023-12-10 23:43:57 -0600

    Day10 Setup

---
+++ Solutions/2023/F#/Tests/Day10-Tests/Tests.fs
@@ -0,0 +1,27 @@
+module Tests
+
+open Xunit
+open Day10
+
+[<Fact>]
+let ``Part1 Example Test`` () = 
+    let input = ParseInput("ExamplePart1.txt")
+    Assert.Equal(4, Part1 input)
+
+[<Fact>]
+let ``Part1 Input Test`` () = 
+    let input = ParseInput("Input.txt")
+    //Assert.Equal(, Part1 input)
+    Assert.Fail("Not implemented")
+
+[<Fact>]
+let ``Part2 Example Test`` () = 
+    let input = ParseInput("ExamplePart2.txt")
+    //Assert.Equal(, Part2 input)
+    Assert.Fail("Not implemented")
+
+[<Fact>]
+let ``Part2 Input Test`` () = 
+    let input = ParseInput("Input.txt")
+    //Assert.Equal(, Part2 input)
+    Assert.Fail("Not implemented")
