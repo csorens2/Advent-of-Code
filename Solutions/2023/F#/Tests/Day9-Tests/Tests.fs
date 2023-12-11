816bab941ee44b09826eb582b8a2b2c32759e830
BearNBird <csorens2@users.noreply.github.com>
2023-12-10 23:37:19 -0600

    Update Tests.fs

--- Solutions/2023/F#/Tests/Day9-Tests/Tests.fs
+++ Solutions/2023/F#/Tests/Day9-Tests/Tests.fs
@@ -6,22 +6,19 @@ open Day9
 [<Fact>]
 let ``Part1 Example Test`` () = 
     let input = ParseInput("ExamplePart1.txt")
-    Assert.Equal(, Part1 input)
+    Assert.Equal(114, Part1 input)
 
 [<Fact>]
 let ``Part1 Input Test`` () = 
     let input = ParseInput("Input.txt")
-    //Assert.Equal(, Part1 input)
-    Assert.Fail("Not implemented")
+    Assert.Equal(1972648895, Part1 input)
 
 [<Fact>]
 let ``Part2 Example Test`` () = 
     let input = ParseInput("ExamplePart2.txt")
-    //Assert.Equal(, Part2 input)
-    Assert.Fail("Not implemented")
+    Assert.Equal(2, Part2 input)
 
 [<Fact>]
 let ``Part2 Input Test`` () = 
     let input = ParseInput("Input.txt")
-    //Assert.Equal(, Part2 input)
-    Assert.Fail("Not implemented")
+    Assert.Equal(919, Part2 input)
