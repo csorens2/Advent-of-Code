54e5758d7e7a3f9dd84e6af5bfca25e438e67263
BearNBird <csorens2@users.noreply.github.com>
2024-01-01 03:07:20 -0600

    Day11 Complete
    Fun diddy

--- Solutions/2023/F#/Day11/Program.fs
+++ Solutions/2023/F#/Day11/Program.fs
@@ -15,42 +15,72 @@ let ParseInput filepath =
             | '.' -> Empty
             | '#' -> Galaxy
             | _ -> failwith $"Failed attempting to parse unknown character: '{nextChar}'")
-        |> Seq.toList)
-    |> Seq.toList
+        |> Seq.toArray)
+    |> Seq.toArray
 
-let Part1 (input: Space list list) = 
+let UniverseDistancesSum universe expansionMultiplier = 
 
-    let inputArray = 
-        input
-        |> List.toArray
-        |> Array.map (fun row -> List.toArray row)
+    let rowCount = Array.length universe
+    let colCount = Array.length universe.[0]
 
-    // First, we get which rows need to be expanded
-    let rowLength = Array.length inputArray
-    let colLength = Array.length inputArray.[0]
+    let rowList = [0..rowCount - 1]
+    let colList = [0..colCount - 1]
 
-    let rowList = [0..rowLength - 1]
-    let colList = [0..colLength - 1]
+    let galaxies = 
+        rowList
+        |> List.fold (fun galaxyPoints row -> 
+            colList
+            |> List.fold (fun subGalaxyPoints col -> 
+                match universe.[row].[col] with 
+                | Empty -> subGalaxyPoints
+                | Galaxy -> (row, col) :: subGalaxyPoints
+            ) galaxyPoints
+        ) List.empty
 
-    let (emptyRow, emptyCol) = 
+    let (emptyRows, emptyCols) = 
         rowList
         |> List.fold (fun (emptyRowAcc, emptyColAcc) yPos -> 
             colList
             |> List.fold (fun (subEmptyRowAcc, subEmptyColAcc) xPos -> 
-                match inputArray.[yPos].[xPos] with 
+                match universe.[yPos].[xPos] with 
                 | Empty -> (subEmptyRowAcc, subEmptyColAcc)
-                | Galaxy -> (Set.remove yPos subEmptyRowAcc, Set.remove xPos subEmptyColAcc)) (emptyRowAcc, emptyColAcc)) (Set.ofList rowList, Set.ofList colList)
+                | Galaxy -> (Set.remove yPos subEmptyRowAcc, Set.remove xPos subEmptyColAcc)
+            ) (emptyRowAcc, emptyColAcc)
+        ) (Set.ofList rowList, Set.ofList colList)
 
-    0
+    let rec getDistanceSum remainingGalaxies currSum =
+        match remainingGalaxies with 
+        | [] -> currSum
+        | [_] -> currSum
+        | nextGalaxy :: galaxiesToCompare -> 
+            let (nextGalaxyY, nextGalaxyX) = nextGalaxy
+            let nextSum = 
+                galaxiesToCompare
+                |> List.map (fun (toCompareY, toCompareX) -> 
+                    let startY = min nextGalaxyY toCompareY
+                    let endY = max nextGalaxyY toCompareY
+                    let startX = min nextGalaxyX toCompareX
+                    let endX = max nextGalaxyX toCompareX
+                    let rowJumps = Set.count (Set.filter (fun emptyRow -> startY <= emptyRow && emptyRow <= endY) emptyRows)
+                    let colJumps = Set.count (Set.filter (fun emptyCol -> startX <= emptyCol && emptyCol <= endX) emptyCols)
 
-let Part2 input = 
-    0
+                    let yDistance = uint64 ((endY - startY) - rowJumps + (rowJumps * expansionMultiplier))
+                    let xDistance = uint64 ((endX - startX) - colJumps + (colJumps * expansionMultiplier))
+                    yDistance + xDistance)
+                |> List.sum
+            getDistanceSum galaxiesToCompare (currSum + nextSum)
+                
+    getDistanceSum galaxies 0UL
+
+let Part1 input = UniverseDistancesSum input 2
+
+let Part2 input = UniverseDistancesSum input 1000000
 
 [<EntryPoint>]
 let main _ =
     let input = ParseInput("Input.txt")
-    //let part1Result = Part1 input
-    //printfn "Part 1 Result: %d" part1Result // 
-    //let part2Result = Part2 input
-    //printfn "Part 2 Result: %d" part2Result // 
+    let part1Result = Part1 input
+    printfn "Part 1 Result: %d" part1Result // 9565386
+    let part2Result = Part2 input
+    printfn "Part 2 Result: %d" part2Result // 857986849428
     0
--- Solutions/2023/F#/Tests/Day11-Tests/Tests.fs
+++ Solutions/2023/F#/Tests/Day11-Tests/Tests.fs
@@ -6,22 +6,24 @@ open Day11
 [<Fact>]
 let ``Part1 Example Test`` () = 
     let input = ParseInput("ExamplePart1.txt")
-    Assert.Equal(374, Part1 input)
+    Assert.Equal(374UL, Part1 input)
 
 [<Fact>]
 let ``Part1 Input Test`` () = 
     let input = ParseInput("Input.txt")
-    //Assert.Equal(, Part1 input)
-    Assert.Fail("Not implemented")
+    Assert.Equal(9565386UL, Part1 input)
 
 [<Fact>]
-let ``Part2 Example Test`` () = 
+let ``Part2-1 Example Test`` () = 
     let input = ParseInput("ExamplePart2.txt")
-    //Assert.Equal(, Part2 input)
-    Assert.Fail("Not implemented")
+    Assert.Equal(1030UL, UniverseDistancesSum input 10)
+
+[<Fact>]
+let ``Part2-2 Example Test`` () = 
+    let input = ParseInput("ExamplePart2.txt")
+    Assert.Equal(8410UL, UniverseDistancesSum input 100)
 
 [<Fact>]
 let ``Part2 Input Test`` () = 
     let input = ParseInput("Input.txt")
-    //Assert.Equal(, Part2 input)
-    Assert.Fail("Not implemented")
+    Assert.Equal(857986849428UL, Part2 input)
