13a4afe7a544ccb47fc375fb549980ad4ece8afe
BearNBird <csorens2@users.noreply.github.com>
2024-02-15 04:48:30 -0600

    Day12 Complete
    Turns out that in 90% of situations, "Dynamic Programming" is just a different phrase for "memoization".
    
    And caching this one was a bitch.

--- Solutions/2023/F#/Day12/Program.fs
+++ Solutions/2023/F#/Day12/Program.fs
@@ -1,64 +1,108 @@
 ﻿module Day12
 
+open System
 open System.IO
 open System.Text.RegularExpressions
 
-type Spring = 
-    | Operational 
-    | Damaged
-    | Unknown
+let OperationalChar = '.'
+let DamagedChar = '#'
+let UnknownChar = '?'
 
 let ParseInput filepath = 
     File.ReadLines(filepath)
     |> Seq.map (fun line ->
         let springs = 
             Regex(@"([#.?]+)").Match(line).Value
-            |> Seq.map(fun springChar ->
-                match springChar with 
-                | '.' -> Operational
-                | '#' -> Damaged
-                | '?' -> Unknown
-                | _ -> failwith $"Failed parsing unknown char {springChar}")
-            |> Seq.toList
         let conditionRecord = 
             Regex(@"(\d+)").Matches(line)
             |> Seq.map (fun recordMatch -> int recordMatch.Groups.[0].Value)
             |> Seq.toList
         (springs, conditionRecord))
-    |> Seq.toList
+
+let CharSeqToString charSeq = 
+    String(Seq.toArray charSeq)
+
+let rec GetSpringVariants springMap (springs: string) damagedGroups = 
+    let remainingDamaged = List.sum damagedGroups
+    match damagedGroups with 
+    | [] ->
+        let mapValue = 
+            if not (springs.Contains(DamagedChar)) then
+                1UL // Our base case representing a valid variant
+            else 
+                0UL
+        Map.add (springs, remainingDamaged) mapValue springMap
+    | nextGroup :: remainingGroups ->
+        if Map.containsKey (springs, remainingDamaged) springMap then 
+            springMap
+        else
+            // We don't care about leading operational springs when processing damaged chains.
+            // But do make sure that the memoization map uses the base spring-string, and not the pruned one
+            let prunedSprings = Seq.skipWhile (fun springChar -> springChar = OperationalChar) springs
+            match Seq.tryHead prunedSprings with 
+            | None -> Map.add (springs, remainingDamaged) 0UL springMap
+            | Some nextSpring when nextSpring = DamagedChar -> 
+                // Start a new damaged chain
+                let rec processGroup (springSeq: seq<char>) remainingDamaged : seq<char> Option = 
+                    match Seq.tryHead springSeq with 
+                    | None when remainingDamaged = 0 -> Some(Seq.empty)
+                    | Some nextSpring when remainingDamaged = 0 && (nextSpring = OperationalChar || nextSpring = UnknownChar) -> Some(Seq.tail springSeq)
+                    | Some nextSpring when nextSpring = DamagedChar || nextSpring = UnknownChar -> processGroup (Seq.tail springSeq) (remainingDamaged - 1)
+                    | _ -> None
+
+                match processGroup prunedSprings nextGroup with 
+                | None -> Map.add (springs, remainingDamaged) 0UL springMap
+                | Some remainingSprings ->
+                    let seqAsString = CharSeqToString remainingSprings
+                    let subMap = GetSpringVariants springMap seqAsString remainingGroups
+
+                    // When dealing with a non-unknown based chain, we just pass up whether the sub-chain was valid
+                    Map.add (springs, remainingDamaged) subMap[(seqAsString, remainingDamaged - nextGroup)] subMap
+
+            | Some nextSpring when nextSpring = UnknownChar -> 
+                let damagedSpringVariant = CharSeqToString (Seq.append [DamagedChar] (Seq.tail prunedSprings))
+                let subDamagedMap = GetSpringVariants springMap damagedSpringVariant damagedGroups
+                let damagedTotal = subDamagedMap[(damagedSpringVariant, remainingDamaged)]
+
+                let operationalSpringVariant = CharSeqToString (Seq.append [OperationalChar] (Seq.tail prunedSprings))
+                let subOperationalMap = GetSpringVariants subDamagedMap operationalSpringVariant damagedGroups
+                let operationalTotal = subOperationalMap[(operationalSpringVariant, remainingDamaged)]
+
+                Map.add (springs, remainingDamaged) (damagedTotal + operationalTotal) subOperationalMap
+            | _ -> failwith "This case should not be reached"
+            
+
+let UnfoldInput (input: seq<(String * int list)>) (foldCount: int) = 
+    input
+    |> Seq.map (fun (springs, groups) -> 
+        let rec expandGroups seperationLambda numLeft toExpand = seq {
+            if numLeft > 1 then
+                yield! seperationLambda toExpand
+                yield! expandGroups seperationLambda (numLeft - 1) toExpand
+            elif numLeft = 1 then
+                yield! toExpand
+        }
+        let expandedSprings = 
+            expandGroups (fun toSeperate -> Seq.append toSeperate [UnknownChar]) foldCount springs
+            |> CharSeqToString
+        let expandedGroups = 
+            expandGroups (id) foldCount groups
+            |> Seq.toList
+        let springVariantsMap = GetSpringVariants Map.empty expandedSprings expandedGroups
+        springVariantsMap[(expandedSprings, List.sum expandedGroups)])
+    |> Seq.sum
 
 let Part1 input = 
-    let rec springVariants remainingSprings remainingGroups currentGroupLength = 
-        match remainingSprings with 
-        | [] ->
-            match remainingGroups with 
-            | [] -> 1
-            | [lastRemainingGroup] when currentGroupLength = lastRemainingGroup -> 1
-            | _ -> 0
-        | nextSpring :: nextRemainingSprings ->
-            let nextGroup = List.tryHead remainingGroups
-            match nextSpring with 
-            | Operational when currentGroupLength = 0 -> springVariants nextRemainingSprings remainingGroups currentGroupLength
-            | Operational when Option.isSome nextGroup && currentGroupLength = nextGroup.Value -> springVariants nextRemainingSprings (List.tail remainingGroups) 0
-            | Operational -> 0
-            | Damaged when Option.isSome nextGroup && currentGroupLength < nextGroup.Value -> springVariants nextRemainingSprings remainingGroups (currentGroupLength + 1)
-            | Damaged -> 0
-            | Unknown -> 
-                (springVariants (Operational :: nextRemainingSprings) remainingGroups currentGroupLength) + 
-                (springVariants (Damaged :: nextRemainingSprings) remainingGroups currentGroupLength)
-
-    input
-    |> List.map (fun (springs, groups) -> springVariants springs groups 0)
-    |> List.sum
+    UnfoldInput input 1
 
 let Part2 input = 
-    0
+    UnfoldInput input 5
 
 [<EntryPoint>]
 let main _ =
     let input = ParseInput("Input.txt")
     let part1Result = Part1 input
     printfn "Part 1 Result: %d" part1Result // 6981
-    //let part2Result = Part2 input
-    //printfn "Part 2 Result: %d" part2Result // 
+    let part2Result = Part2 input
+    printfn "Part 2 Result: %d" part2Result // 4546215031609
     0
--- Solutions/2023/F#/Tests/Day12-Tests/Tests.fs
+++ Solutions/2023/F#/Tests/Day12-Tests/Tests.fs
@@ -6,21 +6,19 @@ open Day12
 [<Fact>]
 let ``Part1 Example Test`` () = 
     let input = ParseInput("Example.txt")
-    Assert.Equal(21, Part1 input)
+    Assert.Equal(21UL, Part1 input)
 
 [<Fact>]
 let ``Part1 Input Test`` () = 
     let input = ParseInput("Input.txt")
-    Assert.Equal(6981, Part1 input)
+    Assert.Equal(6981UL, Part1 input)
 
 [<Fact>]
 let ``Part2 Example Test`` () = 
     let input = ParseInput("Example.txt")
-    //Assert.Equal(, Part2 input)
-    Assert.Fail("Not implemented")
+    Assert.Equal(525152UL, Part2 input)
 
 [<Fact>]
 let ``Part2 Input Test`` () = 
     let input = ParseInput("Input.txt")
-    //Assert.Equal(, Part2 input)
-    Assert.Fail("Not implemented")
+    Assert.Equal(4546215031609UL, Part2 input)
