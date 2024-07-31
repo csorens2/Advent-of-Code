5eb6ab4c9fabf327e77781b90cbf29f3ac214a42
BearNBird <csorens2@users.noreply.github.com>
2024-07-30 23:22:50 -0500

    Setup Day4

---
+++ Solutions/2018/Scala/src/test/scala/Day4Tests.scala
@@ -0,0 +1,28 @@
+
+import Day4.*
+
+class Day4Tests extends munit.FunSuite:
+
+  test("Part1 TestInput") {
+    val input = Day4.parseFile("/Day4/TestInput1.txt")
+    assertEquals(true, false)
+    //val obtained = Part1(input)
+    //val expected = 12
+    //assertEquals(obtained, expected)
+  }
+
+  test("Part1") {
+    assertEquals(true, false)
+    //val input = Day<x>.parseFile("/Day<x>/Input.txt")
+    //val obtained = Part1()
+    //val expected =
+    //assertEquals(obtained, expected)
+  }
+
+  test("Part2") {
+    assertEquals(true, false)
+    //val input = Day<x>.parseFile("/Day<x>/Input.txt")
+    //val obtained = Part2()
+    //val expected =
+    //assertEquals(obtained, expected)
+  }
