349890c308e4eadbfa63bf9183523d135cf1a0f1
BearNBird <csorens2@users.noreply.github.com>
2025-02-01 02:46:38 -0600

    Day5 Complete

---
+++ Solutions/2024/F#/Day5-Tests/Tests.fs
@@ -0,0 +1,24 @@
+﻿module Tests
+
+open Xunit
+open Day5
+
+[<Fact>]
+let ``Part1 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(143, Part1 input)
+
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(6267, Part1 input)
+
+[<Fact>]
+let ``Part2 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(123, Part2 input)
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(5184, Part2 input)
---
+++ Solutions/2024/F#/Day5/Library.fs
@@ -0,0 +1,73 @@
+﻿module Day5
+
+open System.IO
+open System.Text.RegularExpressions
+
+let ParseInput filepath = 
+    let parsePair line = 
+        let lineMatch = (Regex("(\d+)\|(\d+)").Match line)
+        (int lineMatch.Groups[1].Value, int lineMatch.Groups[2].Value)
+    let parseList line =
+        (Regex("(\d+)").Matches line)
+        |> Seq.map (fun lineMatch -> int lineMatch.Groups[1].Value)
+        |> Seq.toList
+
+    let lines = 
+        File.ReadLines(filepath)
+        |> Seq.toList
+
+    let pairs = 
+        lines
+        |> List.filter (fun line -> Regex("(\|)").IsMatch line)
+        |> List.map parsePair
+
+    let lists = 
+        lines
+        |> List.filter (fun line -> Regex("(,)").IsMatch line)
+        |> List.map parseList
+
+    (pairs, lists)
+    
+let ValidateUpdate orderingPairs toValidate = 
+    let updateMap = 
+        toValidate
+        |> List.mapi (fun index value -> (value, index))
+        |> Map.ofList
+
+    let validatePair (left, right) = 
+        if not (updateMap.ContainsKey left) || not (updateMap.ContainsKey right) then 
+            true
+        else
+            Map.find left updateMap < Map.find right updateMap
+    
+    orderingPairs
+    |> List.forall validatePair
+
+let Part1 (input: ((int * int) list * int list list)) = 
+    let (pairs, lists) = input
+    
+    lists
+    |> List.filter (fun update -> ValidateUpdate pairs update)
+    |> List.map (fun update -> update.Item (update.Length / 2))
+    |> List.sum
+
+let Part2 (input: ((int * int) list * int list list)) = 
+    let (pairs, lists) = input
+
+    let sortFunction int1 int2 = 
+        let foundPair = 
+            pairs
+            |> List.tryFind (fun toFind -> toFind = (int1, int2) || toFind = (int2, int1))
+        match foundPair with 
+        | Some((left, right)) -> 
+            if int1 = left then 
+                -1
+            else 
+                1
+        | None -> failwith "Unable to find pair"
+
+    lists
+    |> List.filter (fun update -> not (ValidateUpdate pairs update))
+    |> List.map (fun update -> List.sortWith sortFunction update)
+    |> List.map (fun update -> update.Item (update.Length / 2))
+    |> List.sum
