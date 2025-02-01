54d8bb56429ee722770c5d12eb8ecf415128db8b
BearNBird <csorens2@users.noreply.github.com>
2025-01-31 23:43:51 -0600

    Day2 Complete

---
+++ Solutions/2024/F#/Day2-Tests/Tests.fs
@@ -0,0 +1,24 @@
+﻿module Tests
+
+open Xunit
+open Day2
+
+[<Fact>]
+let ``Part1 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(2, Part1 input)
+
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(670, Part1 input)
+
+[<Fact>]
+let ``Part2 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(4, Part2 input)
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(700, Part2 input)
---
+++ Solutions/2024/F#/Day2/Library.fs
@@ -0,0 +1,56 @@
+﻿module Day2
+
+open System.IO
+open System.Text.RegularExpressions
+
+type Trend = 
+    | Increasing
+    | Decreasing
+
+let ParseInput filepath = 
+    let parseLine line = 
+        Regex(@"(\d+)").Matches(line)
+        |> Seq.map (fun lineMatch -> int lineMatch.Groups.[1].Value)
+        |> Seq.toList
+
+    File.ReadLines(filepath)
+    |> Seq.toList
+    |> List.map parseLine
+
+let rec IsSafe (remainingValues: int list) (prev: Option<int>) (numTrend: Option<Trend>) (usedTolerate: bool) = 
+    if remainingValues.IsEmpty then 
+        true
+    else if not usedTolerate && (IsSafe remainingValues.Tail prev numTrend true) then 
+        true
+    else if prev.IsNone then 
+        IsSafe remainingValues.Tail (Some(remainingValues.Head)) None usedTolerate
+    else if numTrend.IsNone then 
+        if prev.Value < remainingValues.Head then 
+            IsSafe remainingValues prev (Some(Trend.Increasing)) usedTolerate
+        else if prev.Value > remainingValues.Head then 
+            IsSafe remainingValues prev (Some(Trend.Decreasing)) usedTolerate 
+        else
+            false
+    else
+        let nextVal = remainingValues.Head
+        let diff = abs (prev.Value - nextVal)
+        if prev.Value < nextVal && numTrend.Value = Decreasing then 
+            false
+        else if prev.Value > nextVal && numTrend.Value = Increasing then 
+            false
+        else if diff = 0 || diff > 3 then 
+            false
+        else
+            IsSafe remainingValues.Tail (Some(nextVal)) numTrend usedTolerate
+
+let Part1 (input: int list list) = 
+    input
+    |> List.map (fun nums -> IsSafe nums None None true)
+    |> List.filter id
+    |> List.length
+
+let Part2 input = 
+    input
+    |> List.map (fun nums -> IsSafe nums None None false)
+    |> List.filter id
+    |> List.length
