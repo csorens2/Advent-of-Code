c4d6c28663e37dafd751c45491076f97e93d0b6e
BearNBird <csorens2@users.noreply.github.com>
2025-03-09 04:19:20 -0500

    Day15 Part 1 Complete

---
+++ Solutions/2023/F#/Day15/Library.fs
@@ -0,0 +1,23 @@
+﻿module Day15
+
+open System.IO
+
+let ParseInput filepath = 
+    File.ReadLines(filepath)
+    |> Seq.head
+
+let Part1 (input:string) = 
+    let rec processHash (prevValue: int) (remainingString: char List) = 
+        if remainingString.IsEmpty then 
+            prevValue
+        else
+            let nextASCII = int remainingString.Head
+            let nextValue = (((prevValue + nextASCII) * 17) % 256)
+            processHash nextValue remainingString.Tail
+ 
+    input.Split(',')
+    |> Array.map (fun seqString -> processHash 0 (Seq.toList seqString))
+    |> Array.sum
+
+let Part2 input = 
+    0
--- Solutions/2023/F#/Tests/Day15-Tests/Tests.fs
+++ Solutions/2023/F#/Tests/Day15-Tests/Tests.fs
@@ -1,22 +1,21 @@
-module Tests
+﻿module Tests
 
 open Xunit
-open <>
+open Day15
 
 [<Fact>]
 let ``Part1 Example`` () = 
-    let input = ParseInput("Example1.txt")
-    Assert.Equal(, Part1 input)
+    let input = ParseInput("Example.txt")
+    Assert.Equal(1320, Part1 input)
 
 [<Fact>]
 let ``Part1 Input`` () = 
     let input = ParseInput("Input.txt")
-    //Assert.Equal(, Part1 input)
-    Assert.Fail("Not implemented")
+    Assert.Equal(509152, Part1 input)
 
 [<Fact>]
 let ``Part2 Example`` () = 
-    let input = ParseInput("Example2.txt")
+    let input = ParseInput("Example.txt")
     //Assert.Equal(, Part2 input)
     Assert.Fail("Not implemented")
 
