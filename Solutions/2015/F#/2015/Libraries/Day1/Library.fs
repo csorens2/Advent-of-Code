3617ea47e074865aa0ada499b4b8adb09b9433c5
BearNBird <csorens2@users.noreply.github.com>
2025-03-15 01:48:25 -0500

    2015 Day 1 Complete

---
+++ Solutions/2015/F#/2015/Libraries/Day1/Library.fs
@@ -0,0 +1,46 @@
+﻿module Day1
+
+open System.IO
+
+type Instruction = 
+    | Up
+    | Down
+
+let ParseInput filepath = 
+    let mapFunc toMap = 
+        match toMap with 
+        | '(' -> Up
+        | ')' -> Down
+        | _ -> failwith "Unknown char"
+
+    File.ReadLines(filepath)
+    |> Seq.head
+    |> Seq.toList
+    |> List.map mapFunc
+
+let Part1 input = 
+    let mapFunc toMap = 
+        match toMap with 
+        | Up -> 1
+        | Down -> -1
+
+    input
+    |> List.map mapFunc
+    |> List.sum
+
+let Part2 input = 
+    let rec traverseFloors (remainingInstructions: Instruction list) (floor: int) (pos: int) = 
+        if remainingInstructions.IsEmpty then 
+            failwith "Never reached basement"
+        else 
+            let nextInstruction = remainingInstructions.Head
+            let nextFloor = 
+                match nextInstruction with 
+                | Up -> floor + 1
+                | Down -> floor - 1
+            if nextFloor < 0 then 
+                pos
+            else 
+                traverseFloors remainingInstructions.Tail nextFloor (pos + 1)
+
+    traverseFloors input 0 1
---
+++ Solutions/2015/F#/2015/Tests/Day1-Tests/Tests.fs
@@ -0,0 +1,14 @@
+﻿module Tests
+
+open Xunit
+open Day1
+
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(138, Part1 input)
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(1771, Part2 input)
