cf60d9590b449dc6c8c0b662270f8e54b25a3c62
BearNBird <csorens2@users.noreply.github.com>
2025-03-16 02:21:23 -0500

    Day 3 Complete

---
+++ Solutions/2015/F#/Libraries/Day3/Library.fs
@@ -0,0 +1,62 @@
+﻿module Day3
+
+open System.IO
+
+type Direction = 
+    | North
+    | South
+    | East
+    | West
+
+let ParseInput filepath = 
+    let mapChar toMap = 
+        match toMap with 
+        | '^' -> North
+        | 'v' -> South
+        | '>' -> East
+        | '<' -> West
+        | _ -> failwith "Unknown char"
+
+    File.ReadLines(filepath)
+    |> Seq.head
+    |> Seq.map mapChar
+    |> Seq.toList
+
+let DirectionMove direction (startY, startX) = 
+    match direction with 
+    | North -> (startY - 1, startX)
+    | South -> (startY + 1, startX)
+    | East -> (startY, startX + 1)
+    | West -> (startY, startX - 1)
+
+let Part1 input = 
+    let rec processDirections remainingDirections currPos posSet = 
+        if List.isEmpty remainingDirections then 
+            Set.count posSet
+        else
+            processDirections 
+                remainingDirections.Tail 
+                (DirectionMove remainingDirections.Head currPos) 
+                (Set.add currPos posSet)
+    
+    processDirections input (0,0) Set.empty
+    
+
+let Part2 input = 
+    let rec processDirections remainingDirections humanPos robotPos isRobot posSet =
+        if List.isEmpty remainingDirections then 
+            Set.count posSet
+        else   
+            let nextDirection = remainingDirections.Head
+            let (nextHumanPos, nextRobotPos) = 
+                if isRobot then 
+                    (humanPos, DirectionMove nextDirection robotPos) 
+                else 
+                    (DirectionMove nextDirection humanPos, robotPos) 
+            let nextPosSet = 
+                posSet
+                |> Set.add nextHumanPos
+                |> Set.add nextRobotPos
+            processDirections remainingDirections.Tail nextHumanPos nextRobotPos (not isRobot) nextPosSet
+
+    processDirections input (0,0) (0,0) false Set.empty
---
+++ Solutions/2015/F#/Tests/Day3-Tests/Tests.fs
@@ -0,0 +1,14 @@
+﻿module Tests
+
+open Xunit
+open Day3
+
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(2565, Part1 input)
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(2639, Part2 input)
