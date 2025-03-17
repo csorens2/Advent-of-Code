7e779c9182ecbaacad3580609e171828000dc756
BearNBird <csorens2@users.noreply.github.com>
2025-03-17 02:47:22 -0500

    Day 5 Complete

---
+++ Solutions/2015/F#/Libraries/Day5/Library.fs
@@ -0,0 +1,72 @@
+﻿module Day5
+
+open System.IO
+
+let ParseInput filepath = 
+    File.ReadLines(filepath)
+    |> Seq.toList
+
+let Part1 input =         
+    let vowelCheck (toCheck: string) = 
+        let countCharOccurance charToCount = 
+            toCheck
+            |> String.filter(fun nextChar -> nextChar = charToCount)
+            |> String.length
+
+        let vowelCount = 
+            ['a'; 'e'; 'i'; 'o'; 'u']
+            |> List.map countCharOccurance
+            |> List.sum
+        if vowelCount >= 3 then 
+            true
+        else
+            false
+
+    let doubleCheck (toCheck: string) = 
+        ['a'..'z']
+        |> List.exists (fun nextChar -> toCheck.Contains((string nextChar) + (string nextChar)))
+
+    let flaggedCheck (toCheck: string) = 
+        let flaggedCount = 
+            ["ab"; "cd"; "pq"; "xy"]
+            |> List.filter (fun nextString -> toCheck.Contains(nextString))
+            |> List.length
+        if flaggedCount = 0 then 
+            true
+        else
+            false
+            
+    input
+    |> List.filter (fun line -> vowelCheck line && doubleCheck line && flaggedCheck line)
+    |> List.length
+
+let Part2 input = 
+    let check1 (toCheck: string) = 
+        let checkCharPair charPair =
+            if toCheck.Length - (toCheck.Replace(charPair, "").Length) >= 4 then 
+                true
+            else
+                false
+                
+        let charPairs = seq {
+            for i in 'a'..'z' do 
+                for j in 'a'..'z' do 
+                    yield (string i) + (string j)
+        }
+
+        charPairs
+        |> Seq.exists (fun nextPair -> checkCharPair nextPair)
+
+    let check2 (toCheck: string) = 
+        let repeatBetweenStrings = seq {
+            for i in 'a'..'z' do 
+                for j in 'a'..'z' do 
+                    yield (string i) + (string j) + (string i)
+        }
+
+        repeatBetweenStrings
+        |> Seq.exists (fun nextString -> toCheck.Contains(nextString))
+
+    input
+    |> List.filter (fun nextString -> check1 nextString && check2 nextString)
+    |> List.length
---
+++ Solutions/2015/F#/Tests/Day5-Tests/Tests.fs
@@ -0,0 +1,14 @@
+﻿module Tests
+
+open Xunit
+open Day5
+
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(236, Part1 input)
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(51, Part2 input)
