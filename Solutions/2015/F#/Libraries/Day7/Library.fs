8a9df966ad0672ef79e1852b2d17f6a2fd4e2557
BearNBird <csorens2@users.noreply.github.com>
2025-07-13 16:49:34 -0500

    Storing Day 7

--- Solutions/2015/F#/Libraries/Day7/Library.fs
+++ Solutions/2015/F#/Libraries/Day7/Library.fs
@@ -8,8 +8,8 @@ type Instruction =
     | Assign of string*string
     | And of string*string*string
     | Or of string*string*string
-    | LShift of string*string*string
-    | RShift of string*string*string
+    | LShift of string*int*string
+    | RShift of string*int*string
     | Not of string*string
 
 
@@ -33,10 +33,10 @@ let ParseInput filepath =
             Or(lineMatch.Groups[1].Value, lineMatch.Groups[2].Value, lineMatch.Groups[3].Value)
         else if lshiftRegex.IsMatch(line) then 
             let lineMatch = lshiftRegex.Match(line)
-            LShift(lineMatch.Groups[1].Value, lineMatch.Groups[2].Value, lineMatch.Groups[3].Value)
+            LShift(lineMatch.Groups[1].Value, int32 lineMatch.Groups[2].Value, lineMatch.Groups[3].Value)
         else if rshiftRegex.IsMatch(line) then 
             let lineMatch = rshiftRegex.Match(line)
-            RShift(lineMatch.Groups[1].Value, lineMatch.Groups[2].Value, lineMatch.Groups[3].Value)
+            RShift(lineMatch.Groups[1].Value, int32 lineMatch.Groups[2].Value, lineMatch.Groups[3].Value)
         else if notRegex.IsMatch(line) then 
             let lineMatch = notRegex.Match(line)
             Not(lineMatch.Groups[1].Value, lineMatch.Groups[2].Value)
@@ -48,8 +48,78 @@ let ParseInput filepath =
     |> Seq.map mapInstruction
     |> Seq.toList
 
-let Part1 input = 
-    0
+let parseUsableInstructions (valueMap: Map<string, int>) (instructions: Instruction list) =
+    let rec parseInstructions (remaining: Instruction list) (usable: Instruction list) (unusable: Instruction list) = 
+        if List.isEmpty remaining then 
+            (usable, unusable)
+        else
+            let nextInstruction = List.head remaining
+            let instructionUsable = 
+                match nextInstruction with 
+                | Assign(source, _) when Map.containsKey source valueMap -> true
+                | Assign(assignValue, _) when 
+                    let success, value = System.Int32.TryParse(assignValue)
+                    success
+                    -> true
+                | And(source1, source2, _) when Map.containsKey source1 valueMap && Map.containsKey source2 valueMap -> true
+                | Or(source1, source2, _) when Map.containsKey source1 valueMap && Map.containsKey source2 valueMap -> true
+                | LShift(source, _, _) when Map.containsKey source valueMap -> true
+                | RShift(source, _, _) when Map.containsKey source valueMap -> true
+                | Not(source, _) when Map.containsKey source valueMap -> true
+                | _ -> false
+            if instructionUsable then 
+                parseInstructions (List.tail remaining) (nextInstruction :: usable) (unusable)
+            else    
+                parseInstructions (List.tail remaining) (usable) (nextInstruction :: unusable)
+            
+    parseInstructions instructions List.empty List.empty
+
+let Part1 (input: Instruction list) =
+    let rec processInstructions (remaining: Instruction list) (registerValues: Map<string, int>) = 
+        let instructionFold (acc: Map<string,int>) (nextInstruction: Instruction) = 
+            match nextInstruction with 
+            | Assign(source, target) ->
+                let isInt, sourceValue = System.Int32.TryParse(source)
+                if isInt then 
+                    Map.add target sourceValue acc
+                else
+                    let sourceMapValue = Map.find source acc
+                    Map.add target sourceMapValue acc
+            | And(source1, source2, target) ->
+                let source1Value = int32 (Map.find source1 acc)
+                let source2Value = int32 (Map.find source2 acc)
+                let targetValue = source1Value &&& source2Value
+                Map.add target targetValue acc
+            | Or(source1, source2, target) -> 
+                let source1Value = int32 (Map.find source1 acc)
+                let source2Value = int32 (Map.find source2 acc)
+                let targetValue = source1Value ||| source2Value
+                Map.add target targetValue acc
+            | LShift(source, shiftValue, target) ->
+                let sourceValue = Map.find source acc
+                let targetValue = sourceValue <<< shiftValue
+                Map.add target targetValue acc
+            | RShift(source, shiftValue, target) -> 
+                let sourceValue = Map.find source acc
+                let targetValue = sourceValue >>> shiftValue
+                Map.add target targetValue acc
+            | Not(source, target) -> 
+                let sourceValue = Map.find source acc
+                let targetValue = ~~~ sourceValue 
+                Map.add target targetValue acc
+
+
+        if List.isEmpty remaining then 
+            registerValues
+        else
+            let (usableInstructions, nonUsableInstructions) = parseUsableInstructions registerValues remaining
+            let nextRegisterValues = 
+                usableInstructions
+                |> List.fold instructionFold registerValues
+            processInstructions nonUsableInstructions nextRegisterValues
+    
+    let registerMap = processInstructions input Map.empty
+    Map.find "a" registerMap
 
 let Part2 input = 
     0
