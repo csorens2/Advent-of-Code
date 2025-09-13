0baa2510a34fb70b2de4c3de8d5bc974c2ecbdba
BearNBird <csorens2@users.noreply.github.com>
2025-09-13 00:20:29 -0500

    Day 1 Complete

---
+++ Solutions/2022/F#/Libraries/Day1/Library.fs
@@ -0,0 +1,36 @@
+﻿module Day1
+
+open System.IO
+
+let ParseInput filepath = 
+    let rec processLines remainingLines = seq {
+        let nextElfLines = 
+            remainingLines
+            |> List.takeWhile (fun line -> line <> "" ) 
+            |> List.map (fun line -> System.Int32.Parse(line))
+        yield nextElfLines
+        
+        let remainingElfLines = 
+            remainingLines
+            |> List.skipWhile (fun line -> line <> "")
+        if List.tryHead remainingElfLines <> None then 
+            yield! processLines (List.skip 1 remainingElfLines)
+    }
+    
+    File.ReadLines(filepath)
+    |> Seq.toList
+    |> processLines
+    |> Seq.toList
+
+let Part1 (input: int list list) = 
+    input
+    |> List.map (fun elfList -> List.sum elfList)
+    |> List.sortDescending
+    |> List.head
+
+let Part2 (input: int list list) = 
+    input
+    |> List.map (fun elfList -> List.sum elfList)
+    |> List.sortDescending
+    |> List.take 3
+    |> List.sum
---
+++ Solutions/2022/F#/Tests/Day1-Tests/Tests.fs
@@ -0,0 +1,24 @@
+﻿module Tests
+
+open Xunit
+open Day1
+
+[<Fact>]
+let ``Part1 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(24000, Part1 input)
+
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(67633, Part1 input)
+
+[<Fact>]
+let ``Part2 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(45000, Part2 input)
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(199628, Part2 input)
