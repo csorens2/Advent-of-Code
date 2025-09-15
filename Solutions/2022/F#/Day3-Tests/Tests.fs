1a4acd96743d2cb4fbea56f816af37f05c8c8253
BearNBird <csorens2@users.noreply.github.com>
2025-09-14 22:02:12 -0500

    Day 3 Complete

---
+++ Solutions/2022/F#/Day3-Tests/Tests.fs
@@ -0,0 +1,24 @@
+﻿module Tests
+
+open Xunit
+open Day3
+
+[<Fact>]
+let ``Part1 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(157, Part1 input)
+
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(7990, Part1 input)
+
+[<Fact>]
+let ``Part2 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(70, Part2 input)
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(2602, Part2 input)
---
+++ Solutions/2022/F#/Libraries/Day3/Library.fs
@@ -0,0 +1,47 @@
+﻿module Day3
+
+open System.IO
+
+let ParseInput filepath = 
+    File.ReadLines(filepath)
+    |> Seq.map (fun line -> Seq.toList line)
+    |> Seq.toList
+
+let CharToPriority toConvert = 
+    if System.Char.IsLower toConvert then 
+        (int toConvert) - 96
+    else
+        (int toConvert) - 38
+
+let FindCommonElements elementLists = 
+    if List.length elementLists = 0 || List.length elementLists = 1 then 
+        failwith "Insufficient lists to compare"
+    else
+        let elementSets = List.map (fun list -> Set.ofList list) elementLists
+        let commonElementSet = List.fold (fun acc next -> Set.intersect acc next ) elementSets.Head elementSets.Tail
+        Set.toList commonElementSet
+    
+let Part1 input = 
+    let splitList toSplit = 
+        let listLength = List.length toSplit
+        [List.take (listLength / 2) toSplit] @ [List.skip (listLength / 2) toSplit]
+
+    input
+    |> List.map splitList
+    |> List.map FindCommonElements
+    |> List.map (fun ruckSackCommon -> List.sum (List.map CharToPriority ruckSackCommon))
+    |> List.sum
+        
+
+let Part2 input = 
+    let rec elfGroups remainingElves = seq {
+        if not (List.isEmpty remainingElves) then 
+            yield List.take 3 remainingElves
+            yield! elfGroups (List.skip 3 remainingElves)
+    }
+
+    elfGroups input
+    |> Seq.toList
+    |> List.map FindCommonElements
+    |> List.map (fun elfGroup -> List.sum (List.map CharToPriority elfGroup))
+    |> List.sum
