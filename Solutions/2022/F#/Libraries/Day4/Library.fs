e58a26e3a52ff23d1cee6af5badf173c07a93097
BearNBird <csorens2@users.noreply.github.com>
2025-09-14 23:02:37 -0500

    Day 4 Complete

---
+++ Solutions/2022/F#/Libraries/Day4/Library.fs
@@ -0,0 +1,39 @@
+﻿module Day4
+
+open System.IO
+open System.Text.RegularExpressions
+
+
+let ParseInput filepath =
+    let parseLine line = 
+        let lineRegex = Regex(@"((\d+)-(\d+)),((\d+)-(\d+))")
+        let lineMatch = lineRegex.Match line
+        ((int (lineMatch.Groups[2].Value), int (lineMatch.Groups[3].Value)), (int (lineMatch.Groups[5].Value), int (lineMatch.Groups[6].Value)))
+
+    File.ReadLines(filepath)
+    |> Seq.map parseLine
+    |> Seq.toList
+
+let Contains (leftOuter, rightOuter) (leftInner, rightInner) = 
+    if leftOuter <= leftInner && rightInner <= rightOuter then 
+        true
+    else
+        false
+
+let Overlaps (left1, right1) (left2, right2) = 
+    if left1 <= left2 && left2 <= right1 then 
+        true
+    else if left1 <= right2 && right2 <= right1 then 
+        true
+    else 
+        false
+
+let Part1 input =
+    input
+    |> List.filter (fun (left, right) -> (Contains left right) || (Contains right left))
+    |> List.length
+
+let Part2 input = 
+    input
+    |> List.filter (fun (left, right) -> (Overlaps left right) || (Overlaps right left))
+    |> List.length
---
+++ Solutions/2022/F#/Tests/Day4-Tests/Tests.fs
@@ -0,0 +1,24 @@
+﻿module Tests
+
+open Xunit
+open Day4
+
+[<Fact>]
+let ``Part1 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(2, Part1 input)
+
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(475, Part1 input)
+
+[<Fact>]
+let ``Part2 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(4, Part2 input)
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(825, Part2 input)
