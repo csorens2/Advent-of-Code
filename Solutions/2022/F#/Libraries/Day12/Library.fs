6063df02a7993d0f424c7adf3c3e48b5a30e2ef2
BearNBird <csorens2@users.noreply.github.com>
2025-09-29 21:57:13 -0500

    Day 12 Example 1 Complete
    Doing this functionally is impractical. Eventually you just stackoverflow

---
+++ Solutions/2022/F#/Libraries/Day12/Library.fs
@@ -0,0 +1,80 @@
+﻿module Day12
+
+open System.IO
+open FSharpx.Collections
+open System
+
+type QueueEntry = {
+    Visited: Set<int*int>
+    Pos: int*int
+}
+
+let ParseInput filepath = 
+    File.ReadLines(filepath)
+    |> Seq.indexed
+    |> Seq.collect (fun (y, line) -> 
+        line
+        |> Seq.index
+        |> Seq.map (fun (x, gridChar) -> ((y,x), gridChar)))
+    |> Map.ofSeq
+
+let Part1 input = 
+    let startPos = 
+        Map.toList input
+        |> List.filter (fun (_, gridChar) -> gridChar = 'S')
+        |> List.map (fun (gridPos, _) -> gridPos)
+        |> List.head
+
+    let endPos = 
+        Map.toList input
+        |> List.filter (fun (_, gridChar) -> gridChar = 'E')
+        |> List.map (fun (gridPos, _) -> gridPos)
+        |> List.head
+    
+    let rec processTraversalQueue gridMap posQueue = 
+        if Queue.isEmpty posQueue then 
+            failwith "Didn't find top"
+        else
+            let (nextRecord, remainingQueue) = Queue.uncons posQueue
+            let nextPos = nextRecord.Pos
+            
+            if nextPos = endPos then 
+                Set.count nextRecord.Visited
+            elif Set.contains nextPos nextRecord.Visited then 
+                processTraversalQueue gridMap remainingQueue
+            else
+                let nextVisited = Set.add nextRecord.Pos nextRecord.Visited
+                let checkIfCanMove toCheck = 
+                    if not (Map.containsKey toCheck gridMap) then 
+                        false
+                    else
+                        let nextHeight = int gridMap.[nextPos]
+                        let toCheckHeight = int gridMap.[toCheck]
+                        if abs (toCheckHeight - nextHeight) > 1 then 
+                            false
+                        else 
+                            true
+                let traversalFold acc next = 
+                    Queue.conj {QueueEntry.Pos = next; Visited = nextVisited} acc
+
+
+                [
+                        fun (y,x) -> (y+1, x);
+                        fun (y,x) -> (y-1, x);
+                        fun (y,x) -> (y, x+1);
+                        fun (y,x) -> (y, x-1);
+                ]
+                |> List.map (fun traversalFunc -> traversalFunc nextPos)
+                |> List.filter checkIfCanMove
+                |> List.fold traversalFold remainingQueue
+                |> processTraversalQueue gridMap
+                    
+    let startMap = 
+        input
+        |> Map.add startPos 'a' 
+        |> Map.add endPos '{'
+
+    processTraversalQueue startMap (Queue.conj {QueueEntry.Pos = startPos; Visited = Set.empty} Queue.empty)
+
+let Part2 input = 
+    0
---
+++ Solutions/2022/F#/Tests/Day12-Tests/Tests.fs
@@ -0,0 +1,28 @@
+﻿module Tests
+
+open Xunit
+open Day12
+
+[<Fact>]
+let ``Part1 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(31, Part1 input)
+
+(*
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(500, Part1 input)
+
+[<Fact>]
+let ``Part2 Example`` () = 
+    let input = ParseInput("Example.txt")
+    //Assert.Equal(, Part2 input)
+    Assert.Fail("Not implemented")
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    //Assert.Equal(, Part2 input)
+    Assert.Fail("Not implemented")
+*)
