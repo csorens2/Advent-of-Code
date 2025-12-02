36c52c3c86eac60a28b5bd673e5d802a9a89e4f3
BearNBird <csorens2@users.noreply.github.com>
2025-12-02 02:22:18 -0600

    Day 2 Complete

---
+++ Solutions/2025/F#/Libraries/Day2/Library.fs
@@ -0,0 +1,71 @@
+﻿module Day2
+
+open System.IO
+open System.Text.RegularExpressions
+
+let ParseInput filepath =
+    let parseRange range = 
+        let regexMatch = Regex(@"(\d+)-(\d+)").Match(range)
+        (int64 regexMatch.Groups[1].Value, int64 regexMatch.Groups[2].Value)
+    
+    let line = 
+        File.ReadLines(filepath)
+        |> Seq.head
+
+    line.Split [|','|]
+    |> Array.map parseRange
+    |> Array.toList
+
+
+let Part1 (input: (int64*int64) list) = 
+    let processRange (rangeLeft, rangeRight) = 
+        seq {
+            for num in rangeLeft..rangeRight do 
+                let numString = string num 
+                let numLength = String.length numString
+                if numLength % 2 = 0 then 
+                    let leftString = numString.Substring(0, numLength / 2)
+                    let rightString = numString.Substring(numLength / 2)
+                    if leftString = rightString then 
+                        yield num
+        }
+    
+    input 
+    |> List.map processRange
+    |> List.sumBy (fun invalidIds -> Seq.sum invalidIds)
+
+let Part2 (input: (int64*int64) list) = 
+    let processRange (rangeLeft, rangeRight) = 
+        seq {
+            for num in rangeLeft..rangeRight do 
+                let numString = string num 
+                let numLength = String.length numString
+
+                let checkChunkLength chunkLength = 
+                    if numLength % chunkLength <> 0 then 
+                        false
+                    else if chunkLength = numLength then 
+                        false
+                    else
+                        let numChunkArrays = Seq.chunkBySize chunkLength numString
+                        let numStrings = Seq.map (fun (chunkArray: char array) -> System.String chunkArray) numChunkArrays
+                        let toCheck = Seq.head numStrings
+                        if Seq.forall (fun chunkToCheck -> toCheck = chunkToCheck) numStrings then 
+                            true
+                        else 
+                            false
+                
+                let idInvalid = 
+                    [1..numLength]
+                    |> List.map checkChunkLength
+                    |> List.filter id
+                    |> List.isEmpty
+                    |> not
+
+                if idInvalid = true then 
+                    yield num                       
+        }
+
+    input 
+    |> List.map processRange
+    |> List.sumBy (fun invalidIds -> Seq.sum invalidIds)
---
+++ Solutions/2025/F#/Tests/Day2-Tests/Tests.fs
@@ -0,0 +1,24 @@
+﻿module Tests
+
+open Xunit
+open Day2
+
+[<Fact>]
+let ``Part1 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(1227775554L, Part1 input)
+
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(32976912643L, Part1 input)
+
+[<Fact>]
+let ``Part2 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(4174379265L, Part2 input)
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(54446379122L, Part2 input)
