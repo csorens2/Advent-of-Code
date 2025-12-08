e0c88561ad4aeed4481c3b46ae79786c5574c16e
BearNBird <csorens2@users.noreply.github.com>
2025-12-08 01:56:16 -0600

    Day 5 Complete

---
+++ Solutions/2025/F#/Libraries/Day5/Library.fs
@@ -0,0 +1,80 @@
+﻿module Day5
+
+open System.IO
+open System.Text.RegularExpressions
+
+
+let ParseInput filepath = 
+    let rangeRegex = Regex("((\d+)-(\d+))")
+    let idRegex = Regex("^(\d+)$")
+
+    let parseRangeLine line = 
+        let regexMatch = rangeRegex.Match(line)
+        (int64 regexMatch.Groups[2].Value, int64 regexMatch.Groups[3].Value)
+
+    let lines = File.ReadLines(filepath)
+
+    let ranges = 
+        lines
+        |> Seq.filter (fun line -> rangeRegex.IsMatch(line))
+        |> Seq.map parseRangeLine
+        |> Seq.toList
+
+    let ids = 
+        lines
+        |> Seq.filter (fun line -> idRegex.IsMatch(line))
+        |> Seq.map (fun line -> int64 line)
+        |> Seq.toList
+
+    (ranges, ids)
+
+let Part1 input =
+    let (ranges, ids) = input
+
+    let isFresh id = 
+        let foundValidRange = 
+            List.tryFind 
+                (fun (left, right) -> if left <= id && id <= right then true else false) 
+                ranges
+        match foundValidRange with 
+        | Some (_) -> true
+        | None -> false
+    
+    ids
+    |> List.filter isFresh
+    |> List.length
+
+let Part2 input = 
+    let (ranges, _) = input
+
+    let rec mergeRanges remainingRanges = seq {
+        if not (List.isEmpty remainingRanges) then 
+            let (firstRangeLeft, firstRangeRight) = List.head remainingRanges
+
+            let partitionFunction (toCheckLeft, toCheckRight) = 
+                if firstRangeLeft <= toCheckLeft && toCheckLeft <= firstRangeRight then 
+                    true
+                else if firstRangeLeft <= toCheckRight && toCheckRight <= firstRangeRight then 
+                    true
+                else if toCheckLeft <= firstRangeLeft && firstRangeRight <= toCheckRight then 
+                    true
+                else 
+                    false
+            let (canMerge, cantMerge) = List.partition partitionFunction (List.tail remainingRanges)
+
+            if List.isEmpty canMerge then 
+                yield (firstRangeLeft, firstRangeRight)
+                yield! mergeRanges (List.tail remainingRanges)
+            else 
+                let (mergeLeftRange, _) =  List.minBy (fun (left, _) -> left) canMerge
+                let (_, mergeRightRange) = List.maxBy (fun (_, right) -> right) canMerge
+                let newLeftRange = min firstRangeLeft mergeLeftRange
+                let newRightRange = max firstRangeRight mergeRightRange
+                yield! mergeRanges ((newLeftRange, newRightRange)::cantMerge)
+    }
+
+    let mergedRanges = Seq.toList (mergeRanges ranges)
+
+    mergedRanges
+    |> List.map (fun (left, right) -> right - left + 1L)
+    |> List.sum
---
+++ Solutions/2025/F#/Tests/Day5-Tests/Tests.fs
@@ -0,0 +1,24 @@
+﻿module Tests
+
+open Xunit
+open Day5
+
+[<Fact>]
+let ``Part1 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(3, Part1 input)
+
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(681, Part1 input)
+
+[<Fact>]
+let ``Part2 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(14L, Part2 input)
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    Assert.Equal(348820208020395L, Part2 input)
