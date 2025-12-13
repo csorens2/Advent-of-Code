7276066cc39d10ad98d7cfaf0e0ba3032548050a
BearNBird <csorens2@users.noreply.github.com>
2025-12-12 20:13:17 -0600

    Day 7 Setup

---
+++ Solutions/2025/F#/Libraries/Day7/Library.fs
@@ -0,0 +1,25 @@
+﻿module Day7
+
+open System.IO
+
+type Space = 
+    | Open
+    | Start
+    | Splitter
+
+let ParseInput filepath = 
+    let parseLine line = 
+        line
+        |> Seq.map (fun lineChar -> if lineChar = 'S' then Start else if lineChar = '.' then Open else Splitter)
+        |> Seq.toArray
+
+    File.ReadLines(filepath)
+    |> Seq.map parseLine
+    |> Seq.toArray
+
+let Part1 (input: Space array array) = 
+    0
+
+
+let Part2 input = 
+    0
---
+++ Solutions/2025/F#/Tests/Day7-Tests/Tests.fs
@@ -0,0 +1,29 @@
+﻿module Tests
+
+open Xunit
+open Day7
+
+(*
+[<Fact>]
+let ``Part1 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(21, Part1 input)
+
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    //Assert.Equal(, Part1 input)
+    Assert.Fail("Not implemented")
+
+[<Fact>]
+let ``Part2 Example`` () = 
+    let input = ParseInput("Example2.txt")
+    //Assert.Equal(, Part2 input)
+    Assert.Fail("Not implemented")
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    //Assert.Equal(, Part2 input)
+    Assert.Fail("Not implemented")
+*)
