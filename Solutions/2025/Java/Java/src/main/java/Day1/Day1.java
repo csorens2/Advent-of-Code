9ea1b8fba781ff387cad4304233f763bb48f5a71
Csorens2 <csorens2@users.noreply.github.com>
2026-03-21 00:09:59 -0500

    Day 1 Java Complete

--- Solutions/2025/Java/Java/src/main/java/Day1/Day1.java
+++ Solutions/2025/Java/Java/src/main/java/Day1/Day1.java
@@ -1,10 +1,8 @@
 package Day1;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -38,7 +36,7 @@ public class Day1 {
         return new Rotation(finalDirection, Integer.parseInt(matcher.group(2)));
     }
 
-    public List<Rotation> ParseInput(String filepath) throws IOException {
+    public List<Rotation> ParseInput(String filepath) {
         var input = getClass().getClassLoader().getResourceAsStream(filepath);
         var lines = (new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))).lines().toList();
 
@@ -47,11 +45,72 @@ public class Day1 {
                 .toList();
     }
 
-    public int Part1() {
-        return 1;
+    public int Part1(List<Rotation> input) {
+        var pos = 50;
+        var zeroCount = 0;
+
+        for (Rotation rotation : input) {
+            int finalAmount = rotation.amount() % 100;
+            if (rotation.direction() == Left) {
+                if (finalAmount > pos) {
+                    pos = 100 - (finalAmount - pos);
+                }
+                else {
+                    pos -= finalAmount;
+                }
+            }
+            else {
+                if (pos + finalAmount > 99) {
+                    pos = (finalAmount + pos) - 100;
+                }
+                else {
+                    pos += finalAmount;
+                }
+            }
+
+            if (pos == 0) {
+                zeroCount++;
+            }
+        }
+
+        return zeroCount;
     }
 
-    public int Part2() {
-        return 1;
+    public int Part2(List<Rotation> input) {
+        var pos = 50;
+        var zeroCount = 0;
+
+        for (Rotation rotation : input) {
+            int finalAmount = rotation.amount();
+            while (finalAmount > 100) {
+                finalAmount -= 100;
+                zeroCount++;
+            }
+            if (rotation.direction() == Left) {
+                if (finalAmount > pos ) {
+                    if (pos != 0) {
+                        zeroCount++;
+                    }
+
+                    pos = 100 - (finalAmount - pos);
+                }
+                else if (finalAmount == pos) {
+                    zeroCount++;
+                    pos = 0;
+                } else {
+                    pos -= finalAmount;
+                }
+            }
+            else {
+                if (pos + finalAmount > 99) {
+                    zeroCount++;
+                    pos = (finalAmount + pos) - 100;
+                } else {
+                    pos += finalAmount;
+                }
+            }
+        }
+
+        return zeroCount;
     }
 }
--- Solutions/2025/Java/Java/src/test/java/Day1Tests.java
+++ Solutions/2025/Java/Java/src/test/java/Day1Tests.java
@@ -1,14 +1,39 @@
 import Day1.Day1;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
+import static org.junit.Assert.assertEquals;
 
 class Day1Tests {
 
     @Test
-    void Part1TestInput() throws IOException {
+    void Part1TestInput() {
         var day1 = new Day1();
         var input = day1.ParseInput("Day1/TestInput.txt");
-        var test = 1;
+        var result = day1.Part1(input);
+        assertEquals(3, result);
+    }
+
+    @Test
+    void Part1() {
+        var day1 = new Day1();
+        var input = day1.ParseInput("Day1/Input.txt");
+        var result = day1.Part1(input);
+        assertEquals(969, result);
+    }
+
+    @Test
+    void Part2TestInput() {
+        var day1 = new Day1();
+        var input = day1.ParseInput("Day1/TestInput.txt");
+        var result = day1.Part2(input);
+        assertEquals(6, result);
+    }
+
+    @Test
+    void Part2() {
+        var day1 = new Day1();
+        var input = day1.ParseInput("Day1/Input.txt");
+        var result = day1.Part2(input);
+        assertEquals(5887, result);
     }
 }
