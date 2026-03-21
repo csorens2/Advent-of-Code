f01365d3e1f190ba1ec259d66f9035bd35160e21
BearNBird <csorens2@users.noreply.github.com>
2026-03-20 22:47:32 -0500

    Checkin

--- Solutions/2025/Java/Java/src/main/java/Day1/Day1.java
+++ Solutions/2025/Java/Java/src/main/java/Day1/Day1.java
@@ -4,10 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
+
+import static Day1.Direction.Right;
+import static Day1.Direction.Left;
 
 enum Direction {
     Left,
@@ -18,11 +20,31 @@ record Rotation(Direction direction, int amount) {}
 
 public class Day1 {
 
+    public Rotation ProcessLine(String line) {
+        var regex = "([L|R])(\\d+)";
+        var pattern = Pattern.compile(regex);
+        var matcher = pattern.matcher(line);
+
+        matcher.find();
+
+        Direction finalDirection;
+        if (matcher.group(1).equals("R")) {
+            finalDirection = Right;
+        }
+        else {
+            finalDirection = Left;
+        }
+
+        return new Rotation(finalDirection, Integer.parseInt(matcher.group(2)));
+    }
+
     public List<Rotation> ParseInput(String filepath) throws IOException {
         var input = getClass().getClassLoader().getResourceAsStream(filepath);
         var lines = (new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))).lines().toList();
 
-        return Collections.emptyList();
+        return lines.stream()
+                .map(this::ProcessLine)
+                .toList();
     }
 
     public int Part1() {
--- Solutions/2025/Java/Java/src/test/java/Day1Tests.java
+++ Solutions/2025/Java/Java/src/test/java/Day1Tests.java
@@ -3,13 +3,12 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class Day1Tests {
 
     @Test
     void Part1TestInput() throws IOException {
         var day1 = new Day1();
         var input = day1.ParseInput("Day1/TestInput.txt");
+        var test = 1;
     }
 }
