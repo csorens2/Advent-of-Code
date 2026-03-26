710a75eb6e6acbbdde8c45da660a9cecde5baba4
Csorens2 <csorens2@users.noreply.github.com>
2026-03-26 17:47:19 -0500

    2025 C# day 1 early

---
+++ Solutions/2025/C#/2025/Libraries/Day1/Day1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.AccessControl;
+using System.Text.RegularExpressions;
+using static Direction;
+
+public enum Direction
+{
+    Left,
+    Right
+}
+
+namespace Day1
+{
+    
+    public static class Day1
+    {
+        
+
+        public record Rotation(Direction direction, int amount);
+
+        public static Rotation ParseLine(string line)
+        {
+            var regexString = @"([L|R])(\d+)";
+            var match = Regex.Match(line, regexString);
+
+            var amount = int.Parse(match.Groups[2].Value);
+
+            if (match.Groups[1].Value == "R")
+            {
+                return new Rotation(Right, amount);
+            }
+            else
+            {
+                return new Rotation(Left, amount);
+            }
+        }
+
+        public static List<Rotation> ParseInput(string filepath)
+        {
+            return 
+                File.ReadAllLines(filepath)
+                    .Select(line => ParseLine(line))
+                    .ToList();
+        }
+    }
+}
---
+++ Solutions/2025/C#/2025/Tests/Day1Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿namespace Day1Tests
+{
+    public class Day1Tests
+    {
+        [Fact]
+        public void Part1Example()
+        {
+            var input = Day1.Day1.ParseInput("Example.txt");
+
+            var test = 1;
+        }
+    }
+}
