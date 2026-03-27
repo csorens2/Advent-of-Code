d758a3aba5ac0b9bbc606854a54c254751c3e615
BearNBird <csorens2@users.noreply.github.com>
2026-03-26 21:14:15 -0500

    storing

---
+++ Solutions/2025/C#/2025/Libraries/Day1/Day1/Library.cs
@@ -0,0 +1,137 @@
+﻿using System.Security.AccessControl;
+using System.Text.RegularExpressions;
+using static Direction;
+
+public enum Direction
+{
+    Left,
+    Right
+}
+
+namespace Day1
+{
+    
+    public static class Day1
+    {
+        public record Rotation(Direction direction, int amount);
+
+        public static Rotation ParseLine(string line)
+        {
+            var regexString = @"([L|R])(\d+)";
+            var match = Regex.Match(line, regexString);
+
+            var amount = int.Parse(match.Groups[2].Value);
+
+            if (match.Groups[1].Value == "R")
+            {
+                return new Rotation(Right, amount);
+            }
+            else
+            {
+                return new Rotation(Left, amount);
+            }
+        }
+
+        public static List<Rotation> ParseInput(string filepath)
+        {
+            return 
+                File.ReadAllLines(filepath)
+                    .Select(line => ParseLine(line))
+                    .ToList();
+        }
+
+        public static int Part1(List<Rotation> input)
+        {
+            var pos = 50;
+            var zeroCount = 0;
+
+            foreach(Rotation nextRotation in input)
+            {
+                var finalRotation = nextRotation.amount % 100;
+
+                if (nextRotation.direction == Left)
+                {
+                    if (finalRotation > pos)
+                    {
+                        pos = 100 - (finalRotation - pos);
+                    }
+                    else
+                    {
+                        pos = pos - finalRotation;
+                    }
+                }
+                else
+                {
+                    if (pos + finalRotation > 99)
+                    {
+                        pos = (pos + finalRotation) - 100;
+                    }
+                    else
+                    {
+                        pos = pos + finalRotation;
+                    }
+                }
+
+                if(pos == 0)
+                {
+                    zeroCount++;
+                }
+
+            }
+
+            return zeroCount;
+        }
+
+        public static int Part2(List<Rotation> input)
+        {
+            var pos = 50;
+            var zeroCount = 0;
+
+            foreach(Rotation rotation in input)
+            {
+                var finalAmount = rotation.amount;
+                if (finalAmount > 100)
+                {
+                    finalAmount -= 100;
+                    zeroCount++;
+                }
+
+                if (rotation.direction == Left)
+                {
+                    if (finalAmount > pos)
+                    {
+                        if (pos != 0)
+                        {
+                            zeroCount++;
+                        }
+
+                        pos = 100 - (finalAmount - pos);
+                    }
+                    else if (finalAmount == pos)
+                    {
+                        zeroCount++;
+                        pos = 0;
+                    }
+                    else
+                    {
+                        pos -= finalAmount;
+                    }
+                }
+                else
+                {
+                    if (pos + finalAmount > 99)
+                    {
+                        zeroCount++;
+                        pos = (finalAmount + pos) - 100;
+                    }
+                    else
+                    {
+                        pos += finalAmount;
+                    }
+                }
+            }
+
+            return zeroCount;
+        }
+    }
+}
--- Solutions/2025/C#/2025/Tests/Day1Tests/Tests.cs
+++ Solutions/2025/C#/2025/Tests/Day1Tests/Tests.cs
@@ -6,8 +6,32 @@
         public void Part1Example()
         {
             var input = Day1.Day1.ParseInput("Example.txt");
+            var result = Day1.Day1.Part1(input);
+            Assert.Equal(3, result);
+        }
 
-            var test = 1;
+        [Fact]
+        public void Part1()
+        {
+            var input = Day1.Day1.ParseInput("Input.txt");
+            var result = Day1.Day1.Part1(input);
+            Assert.Equal(969, result);
+        }
+
+        [Fact]
+        public void Part2Example()
+        {
+            var input = Day1.Day1.ParseInput("Example.txt");
+            var result = Day1.Day1.Part2(input);
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var input = Day1.Day1.ParseInput("Input.txt");
+            var result = Day1.Day1.Part2(input);
+            Assert.Equal(5887, result);
         }
     }
 }
