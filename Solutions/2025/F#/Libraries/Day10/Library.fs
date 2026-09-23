f730c58927d959402ed50be516e7621f73d8ba79
BearNBird <csorens2@users.noreply.github.com>
2026-09-22 22:22:05 -0500

    Day 10 F# Part 1 Complete

---
+++ Solutions/2025/F#/Libraries/Day10/Library.fs
@@ -0,0 +1,72 @@
+﻿module Day10
+
+open System.IO
+open System.Text.RegularExpressions
+open System.Collections.Immutable
+
+type Machine = {
+    Lights: bool array
+    Buttons: int array array
+    Voltage: int array
+}
+
+let ParseInput filepath = 
+    
+    let parseLine line = 
+        let lineMatch = Regex(@"\[(.*)\] (.*) \{(.*)\}").Match(line)
+        let lights = 
+            lineMatch.Groups[1].Value
+            |> Seq.map (fun lightChar -> if lightChar = '.' then false else true)
+            |> Seq.toArray
+        let mapButtonsAndVoltages bvLine = 
+            Regex(@"(\d+)").Matches(bvLine)
+            |> Seq.map (fun bvMatch -> int bvMatch.Groups[1].Value)
+            |> Seq.toArray
+        let buttons = 
+            lineMatch.Groups[2].Value.Split(' ')
+            |> Seq.map (mapButtonsAndVoltages)
+            |> Seq.toArray
+        let voltage = mapButtonsAndVoltages lineMatch.Groups[3].Value
+        {Machine.Lights = lights; Buttons = buttons; Voltage = voltage}
+
+    File.ReadLines(filepath)
+    |> Seq.map parseLine
+
+let Part1 input = 
+    
+    let processMachine toProcess = 
+        let pressButton lightState button = 
+            Array.fold 
+                (fun lightAcc nextLight -> Array.updateAt nextLight (not lightAcc[nextLight]) lightAcc) 
+                lightState 
+                button
+        
+        let rec bfsToEnd (queue: ImmutableQueue<(bool array * int * Set<int array>)>) = 
+            let (queueLights, queueSteps, queueAvailableButtons) = queue.Peek()
+            let remainingQueue = queue.Dequeue()
+            if queueLights = toProcess.Lights then 
+                queueSteps
+            else
+                let availableButtonsFolder (queueAcc: ImmutableQueue<(bool array * int * Set<int array>)>) nextAvailableButton = 
+                    queueAcc.Enqueue(
+                    (
+                        pressButton queueLights nextAvailableButton,
+                        queueSteps + 1,
+                        Set.remove nextAvailableButton queueAvailableButtons
+                    ))
+
+                bfsToEnd (Set.fold availableButtonsFolder remainingQueue queueAvailableButtons)
+
+        bfsToEnd 
+            (ImmutableQueue.Empty.Enqueue(
+                (
+                    Array.init (Array.length toProcess.Lights) (fun _ -> false),
+                    0,
+                    Set.ofArray toProcess.Buttons
+                )
+            ))
+
+    Seq.sumBy processMachine input
+
+let Part2 input = 
+    0
---
+++ Solutions/2025/F#/Tests/Day10-Tests/Tests.fs
@@ -0,0 +1,27 @@
+﻿module Tests
+
+open Xunit
+open Day10
+
+[<Fact>]
+let ``Part1 Example`` () = 
+    let input = ParseInput("Example.txt")
+    Assert.Equal(7, Part1 input)
+
+[<Fact>]
+let ``Part1 Input`` () = 
+    let input = ParseInput("Input.txt")
+    //Assert.Equal(, Part1 input)
+    Assert.Fail("Not implemented")
+
+[<Fact>]
+let ``Part2 Example`` () = 
+    let input = ParseInput("Example.txt")
+    //Assert.Equal(, Part2 input)
+    Assert.Fail("Not implemented")
+
+[<Fact>]
+let ``Part2 Input`` () = 
+    let input = ParseInput("Input.txt")
+    //Assert.Equal(, Part2 input)
+    Assert.Fail("Not implemented")
